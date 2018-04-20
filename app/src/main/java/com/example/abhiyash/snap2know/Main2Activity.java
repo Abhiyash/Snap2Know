465d5eaa0b3f23887b755b15221a40107d2786e6
Abhiyash <abhiyashm@gmail.com>
2018-04-21 01:51:12 +0530

    Complete app

--- app/src/main/java/com/example/abhiyash/snap2know/Main2Activity.java
+++ app/src/main/java/com/example/abhiyash/snap2know/Main2Activity.java
@@ -2,6 +2,8 @@ package com.example.abhiyash.snap2know;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,8 +15,23 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.net.URI;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 //import com.google.cloud.translate.Translate;
 //import com.google.cloud.translate.TranslateOptions;
@@ -22,12 +39,13 @@ import java.util.List;
 //import com.google.cloud.translate.Detection;
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
-    TextView tv;
+    TextView tv,tv2;
     EditText e;
-    Button b1,b2,b3;
+    Button b1,b2,b3,b4,b5;
     Spinner langsp;
-    String language,lang,text,translatedtext;
-     @Override
+    String language,lang,text,translatedtext,s;
+    File f;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
@@ -36,27 +54,18 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         b1=(Button)findViewById(R.id.button4);
         b2=(Button)findViewById(R.id.button5);
         b3=(Button)findViewById(R.id.button7);
+         b4=(Button)findViewById(R.id.button8);
+         b5=(Button)findViewById(R.id.button9);
          b1.setOnClickListener(this);
         b2.setOnClickListener(this);
          b3.setOnClickListener(this);
+         b4.setOnClickListener(this);
+         b5.setOnClickListener(this);
          tv=(TextView)findViewById(R.id.textView);
-        langsp=(Spinner)findViewById(R.id.spinner1);
+        tv2=(TextView)findViewById(R.id.textView2);
+         langsp=(Spinner)findViewById(R.id.spinner1);
          langsp.setOnItemSelectedListener(this);
-         List<String> categories = new ArrayList<String>();
-         categories.add("German");
-         categories.add("French");
-         categories.add("Spanish");
-         categories.add("Hindi");
-         categories.add("Russian");
 
-         // Creating adapter for spinner
-         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
-
-         // Drop down layout style - list view with radio button
-         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-         // attaching data adapter to spinner
-         langsp.setAdapter(dataAdapter);
         Intent it1=getIntent();
         Bundle ob=it1.getExtras();
          text=ob.getString("text");
@@ -70,21 +79,42 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         {
             case R.id.button4:
                 //Search button
-                if(e.getText().toString().equals(null))
-                {
-                    Toast.makeText(this, "Enter something to search", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-                    //String term = editTextInput.getText().toString();
-                    intent.putExtra(SearchManager.QUERY, e.getText().toString());
-                    startActivity(intent);
-                }
+                List<String> categories2 = new ArrayList<String>();
+                categories2.add("Google");
+
+                // Creating adapter for spinner
+                ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories2);
+
+                // Drop down layout style - list view with radio button
+                dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                // attaching data adapter to spinner
+                langsp.setAdapter(dataAdapter2);
+                langsp.setVisibility(View.VISIBLE);
+                tv.setText("Where do you want to search?");
+                tv.setVisibility(View.VISIBLE);
+                b3.setVisibility(View.VISIBLE);
+
                 break;
             case R.id.button5:
                 //Translate button
+                List<String> categories = new ArrayList<String>();
+                categories.add("German");
+                categories.add("French");
+                categories.add("Spanish");
+                categories.add("Hindi");
+                categories.add("Russian");
+
+                // Creating adapter for spinner
+                ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+                // Drop down layout style - list view with radio button
+                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                // attaching data adapter to spinner
+                langsp.setAdapter(dataAdapter);
                 langsp.setVisibility(View.VISIBLE);
+                tv.setText("Select language");
                 tv.setVisibility(View.VISIBLE);
                 b3.setVisibility(View.VISIBLE);
                 break;
@@ -96,41 +126,126 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
                     lang="de";
                     translatedtext=lt.translateTo(v,lang);
                     Toast.makeText(this, ""+translatedtext, Toast.LENGTH_SHORT).show();
+                    tv.setText(translatedtext);
                 }
                 else if(language.equals("French"))
                 {
                     lang="fr";
                     translatedtext=lt.translateTo(v,lang);
                     Toast.makeText(this, ""+translatedtext, Toast.LENGTH_SHORT).show();
+                    tv.setText(translatedtext);
                 }
                 else if(language.equals("Hindi"))
                 {
                     lang="hi";
                     translatedtext=lt.translateTo(v,lang);
                     Toast.makeText(this, ""+translatedtext, Toast.LENGTH_SHORT).show();
+                    tv.setText(translatedtext);
                 }
                 else if(language.equals("Russian"))
                 {
                     lang="ru";
                     translatedtext=lt.translateTo(v,lang);
                     Toast.makeText(this, ""+translatedtext, Toast.LENGTH_SHORT).show();
+                    tv.setText(translatedtext);
+                }
+                else if(language.equals("Google")) {
+                    if (e.getText().toString().equals(null)) {
+                        Toast.makeText(this, "Enter something to search", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                        //String term = editTextInput.getText().toString();
+                        intent.putExtra(SearchManager.QUERY, e.getText().toString());
+                        startActivity(intent);
+                    }
                 }
                 else
                 {
                     lang="es";
                     translatedtext=lt.translateTo(v,lang);
                     Toast.makeText(this, ""+translatedtext, Toast.LENGTH_SHORT).show();
+                    tv.setText(translatedtext);
                 }
                 break;
+            case R.id.button8:
+                s = e.getText().toString();
+                // Destination Folder and File name
+                String timestamp= new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+                String FILE = Environment.getExternalStorageDirectory().toString()
+                        +"/PDF/"+""+timestamp+".pdf";
+                f=new File(FILE);
+                // Create New Blank Document
+                    Document document = new Document(PageSize.A4);
+                    // Create Directory in External Storage
+                    String root = Environment.getExternalStorageDirectory().toString();
+                    File myDir = new File(root + "/PDF");
+                    myDir.mkdirs();
+                        // Create Pdf Writer for Writting into New Created Document
+                try {
+                    PdfWriter.getInstance(document, new FileOutputStream(FILE));
+                    }
+                    catch (DocumentException e) {
+                    e.printStackTrace();
+                    }
+                    catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                        // Open Document for Writting into document
+                document.open();
+                        // User Define Method
+                addMetaData(document);
+                try {
+                    addTitlePage(document);
+                } catch (DocumentException e) {
+                    e.printStackTrace();
+                }
+                        // Close Document after writting all content
+                document.close();
+
+                Toast.makeText(this, "Pdf Created", Toast.LENGTH_SHORT).show();
+                b5.setVisibility(View.VISIBLE);
+                break;
+            case R.id.button9:
+                Intent sentin=new Intent();
+                sentin.setAction(Intent.ACTION_SEND);
+                sentin.putExtra(Intent.EXTRA_STREAM,Uri.fromFile(f));
+                sentin.setType("application/pdf");
+                startActivity(sentin);
         }
     }
 
+    private void addTitlePage(Document document) throws DocumentException {
+        Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);
+        Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 22, Font.BOLD
+                | Font.UNDERLINE, BaseColor.GRAY);
+        Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
+        Font normal = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL);
+
+
+        Paragraph prProfile = new Paragraph();
+        prProfile.setFont(smallBold);
+        prProfile.add("\n"+s);
+
+        prProfile.setFont(smallBold);
+        document.add(prProfile);
+
+        // Create new Page in PDF
+        document.newPage();
+    }
+
+    private void addMetaData(Document document) {
+
+        document.addTitle("MyPdf");
+        document.addSubject("Person Info");
+        document.addKeywords("Personal, Education, Skills");
+        document.addAuthor("TAG");
+        document.addCreator("TAG");
+    }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
     {
         language=parent.getItemAtPosition(position).toString();
-        Toast.makeText(this, "Selected language "+language, Toast.LENGTH_SHORT).show();
-
+        Toast.makeText(this, "Selected option "+language, Toast.LENGTH_SHORT).show();
     }
 
 
